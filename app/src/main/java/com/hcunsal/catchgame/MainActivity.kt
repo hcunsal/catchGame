796830c9325fbe7875dfe6f8b61ee75266f9c9f6
hcunsal <borutegin@outlook.com>
2022-05-27 21:59:34 +0300

    Character system added.

--- app/src/main/java/com/hcunsal/catchgame/MainActivity.kt
+++ app/src/main/java/com/hcunsal/catchgame/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.hcunsal.catchgame
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+    fun exit(view : View){
+        finish()
+    }
+
+    fun play(view: View){
+        val intent = Intent(applicationContext, GameActivity :: class.java)
+        startActivity(intent)
     }
 }
